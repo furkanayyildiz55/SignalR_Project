0721f398bb4698ecafd13f3924d883a57cad5b98
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-04-20 18:15:06 +0300

    Client olarak kullanılan form uygulamasın üzerinden sunucuda metot tetiklendi

--- SignalRClient.ConsoleApp/Program.cs
+++ SignalRClient.ConsoleApp/Program.cs
@@ -2,22 +2,32 @@
 using SignalRClient.ConsoleApp.Model;
 
 Console.WriteLine("SignalR Console Client ");
+Console.WriteLine("Başlamak için bir tuşa tıklayınız");
 Console.ReadKey();
 
+//Uzak sunucudaki SignalR hub'ına bağlanmak için bir HubConnectionBuilder nesnesi oluşturuyoruz
 var connection = new HubConnectionBuilder()
     .WithUrl("https://localhost:7256/exampleTypeSafeHub")
     .Build();
 
+//Bağlantıyı başlatıyoruz ve bağlantının başarılı olup olmadığını kontrol ediyoruz
 connection.StartAsync().ContinueWith((result) => {
 
-    Console.WriteLine(result.IsCompletedSuccessfully ? "Connected" : "Connnected Failed");
+    Console.WriteLine(result.IsCompletedSuccessfully ? "Connected Successfully" : "Connnected Failed");
 });
 
+//Sunucu tarafından gönderilen mesajları dinlemek için bir olay dinleyici ekliyoruz
 connection.On<Product>("ReceiveTypedMessageForAllClient", (product) => {
 
-    Console.WriteLine($"Received Message:  {product.Id} , {product.Name} , {product.Price}");
+    Console.WriteLine($"Received Product:  {product.Id} , {product.Name} , {product.Price}");
+});
 
-} );
+
+Thread.Sleep(3000);
+Console.WriteLine("Sunucuya Mesaj Göndermek İçin Tıklayın");
+ConsoleKeyInfo keyInfo = Console.ReadKey();
+
+connection.InvokeAsync("BroadcastMessageToClient", "Merhaba Ben FormApplication.");
 
 
 Console.ReadKey();
