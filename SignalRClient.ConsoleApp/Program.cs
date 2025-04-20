f834710b59efa18b45ec33b2a069fef901780faa
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-04-20 16:46:11 +0300

    Console uygulamasını client olarak kullanıp, hub üzerinden mesaj alındı

---
+++ SignalRClient.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using SignalRClient.ConsoleApp.Model;
+
+Console.WriteLine("SignalR Console Client ");
+Console.ReadKey();
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7256/exampleTypeSafeHub")
+    .Build();
+
+connection.StartAsync().ContinueWith((result) => {
+
+    Console.WriteLine(result.IsCompletedSuccessfully ? "Connected" : "Connnected Failed");
+});
+
+connection.On<Product>("ReceiveTypedMessageForAllClient", (product) => {
+
+    Console.WriteLine($"Received Message:  {product.Id} , {product.Name} , {product.Price}");
+
+} );
+
+
+Console.ReadKey();
