75db5e297d840135f0df203b60840f4c7cfaea7d
furkanayyildiz55 <furkanayyildiz55@hotmail.com>
2025-04-22 08:43:12 +0300

    Örnek proje oluşturuldu, paketler, modeller  ve db bağlantısı eklendi

---
+++ SignalR.ExampleProject/Models/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SignalR.ExampleProject.Models;
+
+namespace SignalR.ExampleProject.Models
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) 
+        : IdentityDbContext<IdentityUser,IdentityRole,string>(options)
+    {
+
+        public DbSet<Product> products { get; set; }
+
+    }
+}
---
+++ SignalR.ExampleProject/Models/Products.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalR.ExampleProject.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+
+        [Precision(18, 2)]
+        public decimal Price { get; set; }
+        public string Description { get; set; } = default!;
+        public string UserId { get; set; } = default!;
+    }
+}
+
+
+
+//  = default!; ifadeleri parametrenin default değer alamayacağını belirtir
+//  [Precision(18,2)] 18 karakterlik bir dize olabilir, son iki karakteri 2 hane
